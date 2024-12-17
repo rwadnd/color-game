7553b7c5cec2186e144e719da1412ddf753b5012
Ravad Nadam <0rwad.nadam0@gmail.com>
2024-12-17 08:28:17 +0300

    added the functunality to the game

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-var color = "#32a852"
+var color = "#c8c814"
 var colorText = document.getElementById("text")
 colorText.innerText += " " + color;
 
 document.body.addEventListener("mousemove", track);
 
 function track(e){
-    console.log(e.type);
+    
+    var X = Math.floor(e.clientX / 4)
+    var Y = Math.floor(e.clientY / 2)
+    var string = "rgb("+ X +","+ Y +",20)"
+    console.log(string);
     var body = document.getElementById("body");
-    body.style.backgroundColor = "rgb("+e.clientX+","+e.clientY+",20)"
-    var combination = rgbToHex(e.clientX, e.clientY ,20)
+    body.style.backgroundColor = string
+    var combination = rgbToHex(X, Y ,20)
+    console.log(combination)
+    if(combination === color){
+        alert("You have won!!")
+    }
 }
 
 function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  }
+
+  function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
   }
